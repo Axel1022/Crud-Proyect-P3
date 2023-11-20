af7f651da6c61a88e45827f43bbe3c75b7fdde5e
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2023-11-20 14:49:49 -0400

    Estructura: proyecto base. GC

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIugyOPRm1j8tbcUou4mkAqFKeN9T-pU4",
+  authDomain: "crud-proyect-p3.firebaseapp.com",
+  projectId: "crud-proyect-p3",
+  storageBucket: "crud-proyect-p3.appspot.com",
+  messagingSenderId: "220063668810",
+  appId: "1:220063668810:web:f0c1c00ca0387144ef64b3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+});
+
+const tareaform = document.getElementById("tarea-formulario");
+
+
+tareaform.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  console.log("Holam");
+});
